a8cf4e7ea7a74514a4f1fb974e5e20793d7fd8d7
ClowKhxu <clowkhuux2707@gmail.com>
2025-02-16 12:45:37 +0700

    cap nhat 1

--- src/controller/JWTController.js
+++ src/controller/JWTController.js
@@ -6,27 +6,6 @@ const { handleInsertTokeToCookies } = require('../utils');
 
 const verifyToken = async (req, res, next) => {
     try {
-
-        const ssoToken = req.body?.token
-
-        if (req?.user?.code !== ssoToken) {
-            return res.status(401).json({
-                EC: -1,
-                EM: 'Token không hợp lệ!'
-            })
-        }
-
-        const refreshToken = uuidv4()
-        const response =
-            await JWTService.insertTokenToDB(req?.user?.email, refreshToken, req.body?.typeAccount)
-
-        if (+response?.EC === -1) {
-            return res.status(401).json({
-                EC: response?.EC,
-                EM: response?.EM
-            })
-        }
-
         const payload = {
             id: req?.user?.id,
             username: req?.user?.username,
@@ -37,19 +16,10 @@ const verifyToken = async (req, res, next) => {
             type_account: req?.user?.type_account,
         }
 
-        const accessToken = createJWT(payload)
-
-        // set cookies
-        handleInsertTokeToCookies(res, accessToken, refreshToken)
-
         return res.status(200).json({
             EC: 0,
             EM: 'Xác thực người dùng thành công!',
-            DT: {
-                ...payload,
-                access_token: accessToken,
-                refresh_token: refreshToken
-            }
+            DT: payload
         })
     } catch (error) {
         console.log(error)
